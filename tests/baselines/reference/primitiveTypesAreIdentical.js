b4c7ff92c0d43262fae55faf36e22bb9006e3fc9
Josh Soref <jsoref@users.noreply.github.com>
2016-02-10 20:49:16 +0000

    spelling: primitive

--- tests/baselines/reference/primitiveTypesAreIdentical.js
+++ tests/baselines/reference/primitiveTypesAreIdentical.js
@@ -1,4 +1,4 @@
-//// [primtiveTypesAreIdentical.ts]
+//// [primitiveTypesAreIdentical.ts]
 // primitive types are identical to themselves so these overloads will all cause errors
 
 function foo1(x: number);
@@ -31,7 +31,7 @@ function foo7(x: void);
 function foo7(x: void);
 function foo7(x: any) { }
 
-//// [primtiveTypesAreIdentical.js]
+//// [primitiveTypesAreIdentical.js]
 // primitive types are identical to themselves so these overloads will all cause errors
 function foo1(x) { }
 function foo2(x) { }
